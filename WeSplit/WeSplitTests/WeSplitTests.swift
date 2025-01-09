894dcc6c20b8e2000a51012cd9f8d167e6308eea
nayooti <nayooti@gmail.com>
2025-01-09 18:50:31 +0100

    Setup WeSplit

---
+++ WeSplit/WeSplitTests/WeSplitTests.swift
@@ -0,0 +1,10 @@
+import Testing
+@testable import WeSplit
+
+struct WeSplitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
